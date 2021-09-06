50dbc1a39db8d63869d96ef7433f4831bd7c1dae
Kartik Panjla <kartik198200023@gmail.com>
2021-09-06 15:13:38 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,57 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
-import PriceComponent  from './priceComponent';
+import logo from "./logo.svg";
+import "./App.css";
+import PriceComponent from "./priceComponent";
 
 function App() {
+  const priceDetails = [
+    {
+      type: "FREE",
+      price: "$0",
+      users: "Single User",
+      storage: "5GB",
+      publicProjects: "Unlimited Public Projects",
+      comunityAccess: "Community Access",
+      privateProjects: "Unlimited Private Projects",
+      phoneSupport: "Dedicated Phone Support",
+      subdomain: "Free Subdomain",
+      statusReports: "Monthly Status Reports",
+      availableFeatures: {
+        publicProjects: true,
+        comunityAccess: true,
+        privateProjects: false,
+        phoneSupport: false,
+        subdomain: false,
+        statusReports: false,
+      },
+    },
 
-  const priceDetails=[{
-    type:"FREE",
-    price:"$0",
-    users:"Single User",
-    storage:"5GB Storage",
-    publicProjects:"Unlimited Public Projects",
-    comunityAccess:"Community Access",
-    privateProjects:"Unlimited Private Projects",
-    phoneSupport:"Dedicated Phone Support",
-    subdomain:"Free Subdomain",
-    statusReports:"Monthly Status Reports"
-
-
-},
-
-{
-  type:"PLUS",
-  price:"$9",
-  users:"5 Users",
-  storage:"50GB Storage",
-  publicProjects:"Unlimited Public Projects",
-  comunityAccess:"Community Access",
-  privateProjects:"Unlimited Private Projects",
-  phoneSupport:"Dedicated Phone Support",
-  subdomain:"Free Subdomain",
-  statusReports:"Monthly Status Reports"
-
-
-},
-{
-  type:"PRO",
-  price:"$49",
-  users:"Unlimited Users",
-  storage:"150GB Storage",
-  publicProjects:"Unlimited Public Projects",
-  comunityAccess:"Community Access",
-  privateProjects:"Unlimited Private Projects",
-  phoneSupport:"Dedicated Phone Support",
-  subdomain:"Unlimited Free Subdomain",
-  statusReports:"Monthly Status Reports"
-
-
-}]
+    {
+      type: "PLUS",
+      price: "$9",
+      users: "5 Users",
+      storage: "50GB ",
+      publicProjects: "Unlimited Public Projects",
+      comunityAccess: "Community Access",
+      privateProjects: "Unlimited Private Projects",
+      phoneSupport: "Dedicated Phone Support",
+      subdomain: "Free Subdomain",
+      statusReports: "Monthly Status Reports",
+      availableFeatures: {
+        publicProjects: true,
+        comunityAccess: true,
+        privateProjects: true,
+        phoneSupport: true,
+        subdomain: true,
+        statusReports: false,
+      },
+    },
+    {
+      type: "PRO",
+      price: "$49",
+      users: "Unlimited Users ",
+      storage: "150GB ",
+      publicProjects: "Unlimited Public Projects",
+      comunityAccess: "Community Access",
+      privateProjects: "Unlimited Private Projects",
+      phoneSupport: "Dedicated Phone Support",
+      subdomain: "Unlimited Free Subdomain",
+      statusReports: "Monthly Status Reports",
+      availableFeatures: {
+        publicProjects: true,
+        comunityAccess: true,
+        privateProjects: true,
+        phoneSupport: true,
+        subdomain: true,
+        statusReports: true,
+      },
+    },
+  ];
   return (
     // <div className="App">
     //   <header className="App-header">
 
-        
     //     <img src={logo} className="App-logo" alt="logo" />
     //     <p>
     //       Edit <code>src/App.js</code> and save to reload.
@@ -67,24 +85,24 @@ function App() {
     //   </header>
     // </div>
 
-
-
-
     <>
-<div class="main">
-<div class="container ">
-      <div class="row mt-5 ">
-        <div class="col-4 "> <PriceComponent priceDetail={priceDetails[0]}/></div>
-        <div class="col-4 "> <PriceComponent priceDetail={priceDetails[1]}/></div>
-        <div class="col-4 "><PriceComponent priceDetail={priceDetails[2]}/></div>
+      <div class="main">
+        <div class="container ">
+          <div class="row mt-5 ">
+            <div class="col-4 ">
+              {" "}
+              <PriceComponent priceDetail={priceDetails[0]} />
+            </div>
+            <div class="col-4 ">
+              {" "}
+              <PriceComponent priceDetail={priceDetails[1]} />
+            </div>
+            <div class="col-4 ">
+              <PriceComponent priceDetail={priceDetails[2]} />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-    </div>
-
-    
-   
-   
-    
     </>
   );
 }
--- src/priceComponent.js
+++ src/priceComponent.js
@@ -1,46 +1,66 @@
-import './priceComponent.css';
+import "./priceComponent.css";
 
 function PriceComponent(props) {
+  const features = props.priceDetail;
+  console.log(features);
+  return (
+    <>
+      <div class="card">
+        <div class="card-body">
+          <h6 class="card-subtitle mb-2 text-muted">{features.type}</h6>
+          <div class="card-title">
+            <h1 class="priceHeading">{features.price}</h1>/month
+          </div>
+          <hr />
 
-    const features = props.priceDetail
-    console.log(features)
-    return (<>
-        <div class="card">
-            <div class="card-body">
+          <ul class="list">
+            <li>&#x2714;&nbsp;{features.users} </li>
+            <li>&#x2714;&nbsp;{features.storage} Storage</li>
 
+            {features.availableFeatures.publicProjects ? (
+              <li>&#x2714;&nbsp;{features.publicProjects}</li>
+            ) : (
+              <li class="disable">&#x2716;&nbsp;{features.publicProjects}</li>
+            )}
 
-{/* price PriceComponent */}
-                <h6 class="card-subtitle mb-2 text-muted">{features.type}</h6>
-                <div class="card-title"><h1 class="priceHeading" >{features.price}</h1>/month</div>
-                <hr />
-                <ul class="list">
-                    <li>&#x2714;&nbsp;{features.users}</li>
-                    <li>&#x2714;&nbsp;{features.storage}</li>
-                    <li>&#x2714;&nbsp;{features.publicProjects}</li>
-                    <li>&#x2714;&nbsp;{features.comunityAccess}</li>
+            {features.availableFeatures.comunityAccess ? (
+              <li>&#x2714;&nbsp;{features.comunityAccess}</li>
+            ) : (
+              <li>&#x2716;&nbsp;{features.comunityAccess}</li>
+            )}
 
-                    {features.type === "FREE" ? <><li class="disable">&#x2716;&nbsp;{features.privateProjects}</li>
-                        <li class="disable">&#x2716;&nbsp;{features.phoneSupport}</li>
-                        <li class="disable">&#x2716;&nbsp;{features.subdomain}</li>
-                        <li class="disable">&#x2716;&nbsp;{features.statusReports}</li></> : <><li>&#x2714;&nbsp;{features.privateProjects}</li>
-                        <li >&#x2714;&nbsp;{features.phoneSupport}</li>
-                        <li>&#x2714;&nbsp;{features.subdomain}</li>
+            {features.availableFeatures.privateProjects ? (
+              <li>&#x2714;&nbsp;{features.privateProjects}</li>
+            ) : (
+              <li class="disable">&#x2716;&nbsp;{features.privateProjects}</li>
+            )}
 
+            {features.availableFeatures.phoneSupport ? (
+              <li>&#x2714;&nbsp;{features.phoneSupport}</li>
+            ) : (
+              <li class="disable">&#x2716;&nbsp;{features.phoneSupport}</li>
+            )}
 
-                        {features.type === "PLUS" ? <li class="disable">&#x2716;&nbsp;{features.statusReports}</li> : <li>&#x2714;&nbsp;{features.statusReports}</li>}
-                    </>}
+            {features.availableFeatures.subdomain ? (
+              <li>&#x2714;&nbsp;{features.subdomain}</li>
+            ) : (
+              <li class="disable">&#x2716;&nbsp;{features.subdomain}</li>
+            )}
 
-                </ul>
+            {features.availableFeatures.statusReports ? (
+              <li>&#x2714;&nbsp;{features.statusReports}</li>
+            ) : (
+              <li class="disable">&#x2716;&nbsp;{features.statusReports}</li>
+            )}
+          </ul>
 
-                <button type="button" class="btn btn-primary">Button</button>
-
-
-
-                {/* <p> &#x2714;</p> */}
-
-            </div>
+          <button type="button" class="btn btn-primary">
+            Button
+          </button>
         </div>
-    </>);
+      </div>
+    </>
+  );
 }
 
 export default PriceComponent;
